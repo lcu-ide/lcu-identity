b3b29f69181dcc9c57ca59601eaf3f09411e9cc2
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-02-08 17:00:40 -0700

    Exports

--- projects/common/src/lcu.api.ts
+++ projects/common/src/lcu.api.ts
@@ -1,1 +1,4 @@
+export * from './lib/controls/forgot-password/forgot-password.component';
+export * from './lib/controls/register/register.component';
+export * from './lib/controls/sign-in/sign-in.component';
 export * from './lib/lcu-identity.module';
